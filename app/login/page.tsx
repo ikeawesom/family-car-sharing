dca91852902901659c3d1f9ea92a37e96f291212
ikeawesom <ikelimjl@gmail.com>
2025-04-16 11:58:59 +0800

    added components to pages

---
+++ app/login/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import LoginForm from "@/src/components/LoginForm";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+export default function page() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    const user = sessionStorage.getItem("car-sharing");
+    const odoData = sessionStorage.getItem("odo-data");
+    if (user && odoData) {
+      router.push("/");
+    } else {
+      setLoading(false);
+    }
+  }, []);
+  if (!loading)
+    return (
+      <div className="flex-col flex items-center justify-start w-full gap-y-8">
+        <h1 className="font-bold text-4xl text-white">Lim CarSharing</h1>
+        <LoginForm />
+      </div>
+    );
+  else {
+    return <p className="text-white font-bold">Loading...</p>;
+  }
+}

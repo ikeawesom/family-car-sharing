f9a82b8da1670e2d7d60a67ab69c26c8fd8e124a
ikeawesom <ikelimjl@gmail.com>
2025-04-16 11:58:37 +0800

    added constants

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
+
+export type responseType = {
+  status?: number;
+  errorCode?: number;
+  errorMsg?: string;
+  payload?: any;
+};
+
+export type odoType = {
+  id: number;
+  driver: string;
+  odo_reading: number;
+  created_at: string;
+};
